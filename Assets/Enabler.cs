59292ad99f672a13b232d4a5fcb55eef1605233a
Aadam Kaivo <akaivo@ut.ee>
2018-05-26 22:17:25 +0200

    algus

---
+++ Assets/Enabler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enabler : Photon.MonoBehaviour {
+
+    public GameObject cube;
+
+    public void OnJoinedRoom()
+    {
+        StartCoroutine(delayActivation());
+    }
+
+    private IEnumerator delayActivation()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        cube.SetActive(true);
+        Debug.Log("Activated");
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : Photon.MonoBehaviour {
+
+    [Range(0.01f, 1f)]
+    public float speed = 0.1f;
+
+    private Vector3 direction;
+    
+	void Update ()
+    {
+        if (!photonView.isMine) return;
+
+        direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.A)) direction += Vector3.left;
+        if (Input.GetKey(KeyCode.D)) direction += Vector3.right;
+        if (Input.GetKey(KeyCode.W)) direction += Vector3.forward;
+        if (Input.GetKey(KeyCode.S)) direction += Vector3.back;
+
+        transform.Translate(direction * speed);
+
+        if (Input.GetKey(KeyCode.Space)) transform.position = Vector3.zero;
+
+    }
+}
