b9dc70bde7c4ac8661437ef24e0ac3e78e6142d6
Aadam Kaivo <akaivo@ut.ee>
2018-05-27 12:57:22 +0200

    refactor input

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -4,23 +4,17 @@ using UnityEngine;
 
 public class Move : Photon.MonoBehaviour {
 
-    [Range(0.01f, 1f)]
-    public float speed = 0.1f;
-
-    private Vector3 direction;
+    [Range(1f, 10f)]
+    public float speed = 2f;
     
 	void Update ()
     {
 
         if (!photonView.isMine) return;
 
-        direction = Vector3.zero;
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) direction += Vector3.left;
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) direction += Vector3.right;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) direction += Vector3.forward;
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) direction += Vector3.back;
-
-        transform.Translate(direction * speed);
+        float dx = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float dz = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(dx, 0, dz);
 
         if (Input.GetKey(KeyCode.Space)) transform.position = Vector3.zero;
 
--- Assets/UNetMove.cs
+++ Assets/UNetMove.cs
@@ -5,23 +5,17 @@ using UnityEngine.Networking;
 
 public class UNetMove : NetworkBehaviour {
 
-    [Range(0.01f, 1f)]
-    public float speed = 0.1f;
+    [Range(1f, 10f)]
+    public float speed = 2f;
 
-    private Vector3 direction;
-    
 	void Update ()
     {
 
         if (!isLocalPlayer) return;
 
-        direction = Vector3.zero;
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) direction += Vector3.left;
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) direction += Vector3.right;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) direction += Vector3.forward;
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) direction += Vector3.back;
-
-        transform.Translate(direction * speed);
+        float dx = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float dz = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(dx, 0, dz);
 
         if (Input.GetKey(KeyCode.Space)) transform.position = Vector3.zero;
 
