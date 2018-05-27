e811e55865a5f96a523456b3ae07d4938bc6d891
Aadam Kaivo <akaivo@ut.ee>
2018-05-27 14:20:53 +0200

    gui only for local

--- Assets/SendRateDisplay.cs
+++ Assets/SendRateDisplay.cs
@@ -2,31 +2,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 [RequireComponent(typeof(SendRateChanger))]
-public class SendRateDisplay : MonoBehaviour {
+public class SendRateDisplay : NetworkBehaviour {
 
     private int SendRate;
     private float SendInterval;
 
     private void OnEnable()
     {
+        if (!isLocalPlayer) return;
         GetComponent<SendRateChanger>().OnSendRateChange += updateValues;
     }
 
     private void OnDisable()
     {
+        if (!isLocalPlayer) return;
         GetComponent<SendRateChanger>().OnSendRateChange -= updateValues;
     }
 
     private void updateValues(int newSendRate, float newSendInterval)
     {
+        if (!isLocalPlayer) return;
         SendRate = newSendRate;
         SendInterval = newSendInterval;
     }
 
     private void OnGUI()
     {
+        if (!isLocalPlayer) return;
         GUI.Label(new Rect(10, 5, 200, 20), "SendRate: " + SendRate);
         GUI.Label(new Rect(10, 20, 200, 20), "SendInterval: " + SendInterval);
     }
