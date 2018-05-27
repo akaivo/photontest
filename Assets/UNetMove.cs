01457723443b6a343e7c2f98fd96771995c9badc
Aadam Kaivo <akaivo@ut.ee>
2018-05-27 11:46:25 +0200

    UNet test

---
+++ Assets/UNetMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UNetMove : Photon.MonoBehaviour {
+
+    [Range(0.01f, 1f)]
+    public float speed = 0.1f;
+
+    private Vector3 direction;
+    
+	void Update ()
+    {
+
+        direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.A)) direction += Vector3.left;
+        if (Input.GetKey(KeyCode.D)) direction += Vector3.right;
+        if (Input.GetKey(KeyCode.W)) direction += Vector3.forward;
+        if (Input.GetKey(KeyCode.S)) direction += Vector3.back;
+
+        transform.Translate(direction * speed);
+
+        if (Input.GetKey(KeyCode.Space)) transform.position = Vector3.zero;
+
+    }
+}
