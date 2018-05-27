e969c95da89eab7be7bd1c98bf560726d121e5a9
Aadam Kaivo <akaivo@ut.ee>
2018-05-27 12:41:27 +0200

    add arrow control and increase sync speed

--- Assets/UNetMove.cs
+++ Assets/UNetMove.cs
@@ -16,10 +16,10 @@ public class UNetMove : NetworkBehaviour {
         if (!isLocalPlayer) return;
 
         direction = Vector3.zero;
-        if (Input.GetKey(KeyCode.A)) direction += Vector3.left;
-        if (Input.GetKey(KeyCode.D)) direction += Vector3.right;
-        if (Input.GetKey(KeyCode.W)) direction += Vector3.forward;
-        if (Input.GetKey(KeyCode.S)) direction += Vector3.back;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) direction += Vector3.left;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) direction += Vector3.right;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) direction += Vector3.forward;
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) direction += Vector3.back;
 
         transform.Translate(direction * speed);
 
