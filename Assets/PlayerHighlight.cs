28a4666408a11a014371517ff95082dac118f7a3
Aadam Kaivo <akaivo@ut.ee>
2018-06-08 10:57:40 +0200

    syncing name and uid

---
+++ Assets/PlayerHighlight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHighlight : MonoBehaviour
+{
+    public bool isHighlighted;
+
+    public void Set(bool value)
+    {
+        isHighlighted = value;
+        GetComponent<Renderer>().material.color = value ? Color.red : Color.white;
+    }
+}
---
+++ Assets/PlayerName.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerName : NetworkBehaviour {
+
+    public string deviceName;
+    public string UID;
+
+	void Start ()
+    {
+        Debug.Log("Client call CmdSetNameAndId");
+        CmdSetNameAndId(SystemInfo.deviceName, SystemInfo.deviceUniqueIdentifier);
+	}
+	
+	[Command]
+	private void CmdSetNameAndId(string newName, string newUID)
+    {
+        Debug.Log("Server CmdSetNameAndId run. " + newName);
+        RpcSetNameAndId(newName, newUID);
+	}
+
+    [ClientRpc]
+    private void RpcSetNameAndId(string newName, string newUID)
+    {
+        deviceName = newName;
+        UID = newUID;
+        Debug.LogFormat("RpcSetNameAndId; Name: {0}, UID: {1}", deviceName, UID);
+        gameObject.name = newName;
+    }
+}
