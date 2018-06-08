ab47e7cc35b763678521450add11cc6dc89391f5
Aadam Kaivo <akaivo@ut.ee>
2018-06-08 12:38:22 +0200

    highlight manager only on windows desktops

--- Assets/SpawnHighlightManager.cs
+++ Assets/SpawnHighlightManager.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class SpawnHighlightManager : NetworkBehaviour {
-
+public class SpawnHighlightManager : NetworkBehaviour
+{
+    
     public GameObject highLightManagerPrefab;
 
     void Start()
     {
-    CmdSpawnHighlightManager(); //THIS SENDS A REQUEST TO THE SERVER TO SPAWN A MANAGER.
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+        CmdSpawnHighlightManager();
+#endif
     }
 
-    [Command]    //LIKE WITH ANY COMMAND THIS CODE IS ONLY EXECUTED ON THE SERVER.
+    [Command]
     public void CmdSpawnHighlightManager()
     {
-        GameObject manager = Instantiate(highLightManagerPrefab) as GameObject; //SpawnWithClientAuthority WORKS JUST LIKE NetworkServer.Spawn ...THE
-                                                                                                            //GAMEOBJECT MUST BE INSTANTIATED FIRST.
-
-        NetworkServer.SpawnWithClientAuthority(manager, gameObject); //THIS WILL SPAWN THE troop THAT WAS CREATED ABOVE AND GIVE AUTHORITY TO THIS PLAYER. THIS PLAYER (GAMEOBJECT) MUST
-                                                                   //HAVE A NetworkIdentity ON IT WITH Local Player Authority CHECKED.
+        GameObject manager = Instantiate(highLightManagerPrefab) as GameObject; 
+        NetworkServer.SpawnWithClientAuthority(manager, gameObject);
     }
 }
