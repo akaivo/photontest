b93063a16b659def4bca765e7000ef3d0b0341e1
Aadam Kaivo <akaivo@ut.ee>
2018-07-07 18:08:47 +0200

    put users in same room

--- Assets/Photon Unity Networking/UtilityScripts/ConnectAndJoinRandom.cs
+++ Assets/Photon Unity Networking/UtilityScripts/ConnectAndJoinRandom.cs
@@ -49,22 +49,14 @@ public class ConnectAndJoinRandom : Photon.MonoBehaviour
         Debug.Log("OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room. Calling: PhotonNetwork.JoinRandomRoom();");
         PhotonNetwork.AuthValues = new AuthenticationValues();
         PhotonNetwork.AuthValues.UserId = SystemInfo.deviceUniqueIdentifier;
-        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinOrCreateRoom("photontest", new RoomOptions() { MaxPlayers = 10 }, null);
     }
 
     public virtual void OnJoinedLobby()
     {
         Debug.Log("OnJoinedLobby(). This client is connected and does get a room-list, which gets stored as PhotonNetwork.GetRoomList(). This script now calls: PhotonNetwork.JoinRandomRoom();");
-        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinOrCreateRoom("photontest", new RoomOptions() { MaxPlayers = 10 }, null);
     }
-
-    public virtual void OnPhotonRandomJoinFailed()
-    {
-        Debug.Log("OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one. Calling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
-        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 4 }, null);
-    }
-
-    // the following methods are implemented to give you some context. re-implement them as needed.
 
     public virtual void OnFailedToConnectToPhoton(DisconnectCause cause)
     {
@@ -73,6 +65,6 @@ public class ConnectAndJoinRandom : Photon.MonoBehaviour
 
     public void OnJoinedRoom()
     {
-        Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room. From here on, your game would be running. For reference, all callbacks are listed in enum: PhotonNetworkingMessage");
+        Debug.Log("OnJoinedRoom()");
     }
 }
